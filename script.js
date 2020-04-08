9f288eb3165b7bab32419a5f1d72295733c5a079
debo9210 <omololuadebowale1@gmail.com>
2020-04-08 04:57:11 -0400

    background generator

---
+++ script.js
@@ -0,0 +1,15 @@
+//jshint esversion:9
+
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const css = document.querySelector('h3');
+const body = document.querySelector('#gradient');
+
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
+
+function setGradient() {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    css.textContent = `${body.style.background};`;
+}
